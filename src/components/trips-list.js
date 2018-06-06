21b95f1519bae52ec8777c8e8c4f0655bf27a81b
Alexa Scott <arscott4792@gmail.com>
2018-06-06 19:30:23 -0400

    new trip now renders on page!!

--- src/components/trips-list.js
+++ src/components/trips-list.js
@@ -6,34 +6,36 @@ import { addtrip } from '../actions/trips';
 import TripRow from './trip-row';
 
 export default class TripsList extends React.Component {
-    constructor(props, context) {
-        super(props, context);
+    // constructor(props, context) {
+    //     super(props, context);
 
-        const ds = new ListView.DataSource({
-            rowHasChanged: (r1, r2) => r1 !== r2
-        });
+    //     const ds = new ListView.DataSource({
+    //         rowHasChanged: (r1, r2) => r1 !== r2
+    //     });
 
-        this.state = {
-            dataSource: ds.cloneWithRows(props.trips)
-        };
-    }
+    //     this.state = {
+    //         dataSource: ds.cloneWithRows(props.trips)
+    //     };
+    // }
 
-    renderRow(trip) {
-        return (
-            <TripRow trip={trip} nav={this.props.nav} />
-        )
-    }
+    // renderRow(trip) {
+    //     return (
+    //         <TripRow trip={trip} nav={this.props.nav} />
+    //     )
+    // }
     
     render() {
-        //const tripsArr = this.props.trips.map(trip => <Text>{trip}</Text>);
+        const tripsArr = this.props.trips.map(trip => <Text>{trip}</Text>);
         // console.log('state logging from TripList', this.state);
         // console.log('TripLists Comp props:', tripsArr);
         return (
             <View>
-                <ListView
-                    dataSource={this.state.dataSource}
-                    renderRow={this.renderRow.bind(this)}>
-                </ListView>
+                {/* <ListView> */}
+                    {tripsArr}
+                    {/* <TripRow trip={trip} nav={this.props.nav} /> */}
+                    {/* // dataSource={this.state.dataSource}
+                    // renderRow={this.renderRow.bind(this)}> */}
+                {/* </ListView> */}
             </View>
         );
     }
